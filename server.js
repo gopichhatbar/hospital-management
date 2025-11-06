6ddfa26a90ca02c4c74e21ae11f332852bda9326
gopichhatbar <chhatbargopi74@gmail.com>
2025-11-06 13:47:26 +0530

    db changes

--- server.js
+++ server.js
@@ -5,32 +5,31 @@ const dotenv = require("dotenv");
 dotenv.config();
 const path = require("path");
 
+// Database connection
+require("./config/database");
 
-require("./config/database")
-const addhospital = require("./api/addhospital")
-const addDoctor = require("./api/addDoctor")
-const authenticate = require("./api/authentication")
-const emailRoutes = require('./api/appointment'); // path to the file above
-
-
+// Routes
+const addhospital = require("./api/addhospital");
+const addDoctor = require("./api/addDoctor");
+const authenticate = require("./api/authentication");
+const emailRoutes = require("./api/appointment");
 
 const app = express();
+
+// Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.json()); // Increase JSON limit
-app.use(express.urlencoded({  extended: true })); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
+// Routes
+app.get("/", (req, res) => res.send("Node.js Backend is Running!"));
+app.use("/addhospital", addhospital);
+app.use("/", addDoctor);
+app.use("/authenticate", authenticate);
+app.use("/mailsent", emailRoutes);
+
+// Start server
 const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) => {
-  res.send("Node.js Backend is Running!");
-});
-app.use("/addhospital",addhospital);
-app.use("/",addDoctor);
-app.use("/authenticate",authenticate);
-app.use("/mailsent",emailRoutes)
-
-
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
